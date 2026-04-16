06a2b09bfc4091f8c66f3ca42318a3a0f45081c2
WilsonPsychedelic <michaelbrooks452@gmail.com>
2026-04-16 07:46:01 -0700

    Modified .py and .html files

--- stat_pgs_tmpl/views.py
+++ stat_pgs_tmpl/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'stat_pgs_tmpl/home.html')
+    ctx = {
+        'name': "Tony",
+        'age': 25,
+        'gains': 2344.322211
+    }
+    return render(request, 'stat_pgs_tmpl/home.html', ctx)
 
 def contact(request):
     return render(request, 'stat_pgs_tmpl/contact.html')
